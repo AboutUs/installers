b60d894a1d6eed81d30df757866a1946075f78d2
Didip Kerabat <didip@aboutus.org>
2010-03-18 12:02:23 -0700

    added karl's installer

---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# ------------------------------------------------------------------------------
+# Simple installer script to configure Ubuntu 9.10 images on EC2
+# ------------------------------------------------------------------------------
+
+if [ "x$(id -u)" != "x0" ]; then
+	echo "You must be root to run this script (try sudo)"
+	exit 1
+fi
+
+apt-get update
+
+PKGS="ruby irb git-core rake rubygems chef curl zip unzip openjdk-6-jre-headless sysstat mysql-client jruby1.2 ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb build-essential wget ssl-cert ohai httpd mlocate libxml2-dev libxslt-dev libcurl3 mysql mysql-server mysql-dev libmysqlclient15-dev apache2-mpm-prefork libcurl4-openssl-dev ntp librspec-ruby1.8"
+
+for name in $PKGS; do
+	apt-get -y install $name
+done
