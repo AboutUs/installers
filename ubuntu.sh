6ea0af252968930ba198847f21f7975c8b2c6ed0
Didip Kerabat <didip@aboutus.org>
2010-03-18 13:48:22 -0700

    the one installer for ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-sudo apt-get update
-sudo apt-get upgrade --show-upgraded
+# ------------------------------------------------------------------------------
+# Simple installer script to configure Ubuntu 9.10 images on EC2
+# ------------------------------------------------------------------------------
 
-sudo apt-get install -y git
-sudo apt-get install -y git-core
-sudo apt-get install -y httpd
-sudo apt-get install -y ruby irb
-sudo apt-get install -y rubygems
-sudo apt-get install -y ruby1.8-dev
-sudo apt-get install -y mlocate
-sudo apt-get install -y libopenssl-ruby
-sudo apt-get install -y libxml2-dev
-sudo apt-get install -y libxslt-dev
-sudo apt-get install -y libcurl3-dev libghc6-curl-dev libcurl-dev libflickcurl-dev
-sudo apt-get install -y mysql-server libmysqlclient15-dev
+if [ "x$(id -u)" != "x0" ]; then
+	echo "You must be root to run this script (try sudo)"
+	exit 1
+fi
+
+apt-get update
+apt-get upgrade --show-upgraded
+
+PKGS="git-core ruby rake rubygems irb chef curl zip unzip httpd openjdk-6-jre-headless sysstat mysql-client jruby1.2 ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb build-essential wget ssl-cert ohai httpd mlocate libxml2-dev libxslt-dev libcurl3 mysql mysql-server mysql-dev libmysqlclient15-dev apache2-mpm-prefork libcurl4-openssl-dev ntp librspec-ruby1.8"
+
+for name in $PKGS; do
+	apt-get -y install $name
+done
